9a6cc59997875bc1c714e3c0033f224f55a28651
Uthmanbello <uthmanbello88@gmail.com>
2023-09-27 14:31:59 +0100

    Add styles folder and navbar.css file

--- src/App.js
+++ src/App.js
@@ -1,30 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// App.tsx
 import React from 'react';
 import Navbar from './Navbar';
 
@@ -32,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      {/* The rest of your content */}
     </div>
   );
 }
