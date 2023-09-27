d53f551c5018afd60199db79b8355cdc18bc9f12
Uthmanbello <uthmanbello88@gmail.com>
2023-09-27 18:55:42 +0100

    Style dropdown button

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,17 +5,18 @@ import disqualified from './images/disqualified.svg';
 import qualified from './images/qualified.svg';
 import reachout from './images/reachout.svg';
 import mail from './images/mail.svg';
+import blueArrowdown from './images/blue-arrowdown.svg';
 
 export function Homepage() {
   return (
-    <div className="homepage row space-between">
+    <div className="homepage row space-between align-center">
       <div className='column'>
         <p className='blue heading'>London Internship Program</p>
-        <p className='medium-text'>London</p>
+        <p className='medium-text heading-below'>London</p>
       </div>
 
       <div className="dropdown">
-        <button className="dropbtn">Opportunity Browsing</button>
+        <button className="dropbtn blue space-between">Opportunity Browsing <img src={blueArrowdown} alt='arrow down'></img></button>
         <div className="dropdown-content">
           <a href="#">
             <p>Applied</p>
