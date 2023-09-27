9d3625a21f626bf52bb7ccd9ec1605ed88b63982
Uthmanbello <uthmanbello88@gmail.com>
2023-09-27 17:52:28 +0100

    Add styling to homepage heading

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Homepage from './Homepage';
 import './styles/App.css'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Homepage />
     </div>
   );
 }
