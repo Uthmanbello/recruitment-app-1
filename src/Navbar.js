552a8e5dd95772f82997b68da447f117dc5c583d
Uthmanbello <uthmanbello88@gmail.com>
2023-09-27 16:46:00 +0100

    Add the remaining icons to the navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './styles/Navbar.css'
 import homeIcon from './images/home-icon.svg'
+import peopleIcon from './images/people-icon.svg';
+import taskIcon from './images/task-icon.svg';
+import shareIcon from './images/share-icon.svg';
+import fileIcon from './images/file-icon.svg';
+import recordIcon from './images/record-icon.svg';
+import heartIcon from './images/heart-icon.svg';
+import arrowleftIcon from './images/arrowleft-icon.svg';
 
 function Navbar() {
   return (
     <div className="navbar">
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
         <li><img src={homeIcon} alt='home icon'></img></li>
+        <li><img src={peopleIcon} alt='people icon'></img></li>
+        <li><img src={taskIcon} alt='task icon'></img></li>
+        <li><img src={shareIcon} alt='share icon'></img></li>
+        <li><img src={fileIcon} alt='file icon'></img></li>
+        <li><img src={recordIcon} alt='record icon'></img></li>
+        <li><img src={heartIcon} alt='heart icon'></img></li>
+        <li><img src={arrowleftIcon} alt='arrow left icon'></img></li>
       </ul>
     </div>
   );
