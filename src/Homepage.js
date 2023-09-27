b65ace917969b9824fe27783309bed03400b832c
Uthmanbello <uthmanbello88@gmail.com>
2023-09-27 20:16:55 +0100

    Style video button

--- src/Homepage.js
+++ src/Homepage.js
@@ -6,10 +6,11 @@ import qualified from './images/qualified.svg';
 import reachout from './images/reachout.svg';
 import mail from './images/mail.svg';
 import blueArrowdown from './images/blue-arrowdown.svg';
+import whiteArrowdown from './images/white-arrowdown.svg';
 
 export function Homepage() {
   return (
-    <div className="homepage row space-between align-center">
+    <div className="homepage row align-center">
       <div className='column'>
         <p className='blue heading'>London Internship Program</p>
         <p className='medium-text heading-below'>London</p>
@@ -17,12 +18,12 @@ export function Homepage() {
 
       <div className="dropdown">
         <button className="dropbtn blue space-between">Opportunity Browsing <img src={blueArrowdown} alt='arrow down'></img></button>
-        <div className="dropdown-content">
+        {/* <div className="dropdown-content">
           <a href="#">
             <p>Applied</p>
             <p>1745</p>
           </a>
-        </div>
+        </div> */}
       </div>
 
       <div className='heading-icons'>
@@ -33,7 +34,7 @@ export function Homepage() {
         <img src={mail} alt='mail icon'></img>
       </div>
 
-      <button className="videobtn">Move To Video Interview I</button> 
+      <button className="videobtn space-between">Move To Video Interview I<img src={whiteArrowdown} alt='arrow down'></img></button> 
     </div>
   );
 }
